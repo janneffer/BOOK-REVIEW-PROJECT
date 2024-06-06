dcf52d033ee697568f614cfca16c89d51aa454d8
Nicole R <130779234+nicolergn@users.noreply.github.com>
2024-06-06 23:02:00 +0800

    Update home.php

--- sections/home.php
+++ sections/home.php
@@ -1,14 +1,15 @@
 <!-- ==================== HOME SECTION ==================== -->
 <section class="home" id="home">
-            <img src="asset/picture/home_bg.jpg" alt="Catching Fire">
-            <div class="home-text">
-                <h1>ReadRater</h1>
-            </div>
-            <form>
-                <div class="search">
-                    <i class='bx bx-search'></i>
-                    <i class='bx bx-x clear-icon'></i>
-                    <input type="text" placeholder="Search Book">
-                </div>
-            </form>
-        </section>
+    <img src="asset/picture/home_bg.jpg" alt="Catching Fire">
+    <div class="home-text">
+        <h1>ReadRater</h1>
+    </div>
+    <form action="browse.php" method="GET"> <!-- Tambahkan action dan method pada form -->
+        <div class="search">
+            <i class='bx bx-search'></i>
+            <i class='bx bx-x clear-icon'></i>
+            <input type="text" name="search" placeholder="Search Book">
+        </div>
+        <button type="submit" style="display: none;">Submit</button>
+    </form>
+</section>
